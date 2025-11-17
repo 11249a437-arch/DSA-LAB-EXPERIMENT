9dc51f8de1a933093ce689829b335919c061fe5d
11249a437-arch <11249a437@kanchiuniv.ac.in>
2025-11-17 20:43:54 +0530

    Fix addition program and correct variable usage

--- dsa 1.c
+++ dsa 1.c
@@ -1,13 +1,17 @@
 ADDITION OF TWO NUMBERS
 
+#include <stdio.h>
+#include <math.h>
 
-#include<stdio.h>
-#include<math.h>
 int main()
 {
- int num1,num2;
-printf("enter two numbers:");
-scanf("%d","%d",&sum1,&sum2);
-printf("addition of %d&%d numbers is :%d",num1,num2,num1+num2);
-return 0;
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    printf("Addition of %d & %d is: %d\n", num1, num2, num1 + num2);
+    return 0;
 }
+
+output
+Enter two numbers: 40 60
+Addition of 40 & 60 is: 100
