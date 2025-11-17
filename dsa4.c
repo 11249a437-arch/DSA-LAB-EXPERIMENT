fefb367d6a0efee157cc9c52f0bb6e00d71c402a
11249a437-arch <11249a437@kanchiuniv.ac.in>
2025-11-17 20:54:30 +0530

    Update dsa4.c

--- dsa4.c
+++ dsa4.c
@@ -1,44 +1,51 @@
 BINARY SEARCH
 
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-int n,i,key;
-printf("enter the size of array:");
-scanf("%d",&n);
-int a[n];
-printf("enter the element in array:");
-for(i=0;i<n;i++)
-{
-scanf("%d",&a[i]);
+    int n, i, key;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enter the elements in array (sorted):\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    int start = 0, end = n - 1, mid;
+
+    while(start <= end)
+    {
+        mid = (start + end) / 2;
+
+        if(a[mid] == key)
+        {
+            printf("Element found at index %d\n", mid);
+            return 0;
+        }
+        else if(a[mid] < key)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+
+    printf("Element not found\n");
+    return 0;
 }
-printf(" enter the element to search:");
-for(i=0,i<n;i++)
-{
-scanf("%d",&key);
-}
-}
-int start,end,mid;
-start=0;
-end=n-1;
-while(start<=end);
-{
-mid=[start+end]/2;
-if(a[mid]==key)
-{
- printf("found the position of mid");
-return 0;
-}
-else
-if(a[mid]<key)
-{
-start=mid+1;
-}
-else if(a[mid]>key)
-{
-end=mid-1;
-}
-}
-return 0;
-}
-}
+output
+Enter the size of array: 5
+Enter the elements in array (sorted):
+10 20 30 40 50
+Enter the element to search: 40
+Element found at index 3
